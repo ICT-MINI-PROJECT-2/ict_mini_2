2bc36ccd6d5244271eb9658577eff6d99513732f
Gunmo Lee <rjsah5676@gmail.com>
2025-03-05 18:20:13 +0900

    first

---
+++ serv/src/main/java/com/ke/serv/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.ke.serv.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
