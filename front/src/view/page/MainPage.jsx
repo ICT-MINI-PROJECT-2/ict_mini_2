74167fc829ae74a47fe4a926ac3f62bebb101478
Gunmo Lee <rjsah5676@gmail.com>
2025-03-06 13:08:24 +0900

    server setting

--- front/src/view/page/MainPage.jsx
+++ front/src/view/page/MainPage.jsx
@@ -1,11 +1,22 @@
+import {useState} from 'react';
 import '../../css/page/mainpage.css';
-import Faded from '../../effect/Faded'
-function MainPage(){
+import Faded from '../../effect/Faded';
+import axios from 'axios';
 
+function MainPage(){
+    const [tt,setTt] = useState('');
+    const test = () =>{
+        axios.get('http://localhost:9977/test')
+        .then(res => {
+            setTt(res.data);
+        })
+        .catch(err => console.log(err));
+    }
     return(
         <Faded>
             <div className="main-container">
-                main
+                <button onClick={test} style={{marginTop:'300px'}}>TEST</button>
+                {tt}
             </div>
         </Faded>
     )
--- serv/src/main/java/com/ke/serv/controller/HomeController.java
+++ serv/src/main/java/com/ke/serv/controller/HomeController.java
@@ -1,12 +1,19 @@
 package com.ke.serv.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HomeController {
     @GetMapping("/")
     public String home(){
         return "index";
+    }
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("hello");
+        return "hello!";
     }
 }
