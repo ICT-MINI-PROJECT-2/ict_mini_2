ab59347917fa71d09f91d52a5b43434f6d6f24da
drumtiger <astrallana@naver.com>
2025-03-11 15:11:49 +0900

    find 페이지 list 표출 구현

--- front/src/view/page/find/Find.jsx
+++ front/src/view/page/find/Find.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import '../../../css/page/find/find.css';
 import Faded from '../../../effect/Faded';
 import plusImg from '../../../img/plus.png';
 import searchImg from '../../../img/search.png';
+import FindListItem from './FindListItem';
+import axios from 'axios';
 
 function Find(){
+    let [list, setList] = useState([]);
+
+    function searchList() {
+        axios.get('http://localhost:9977/find/searchList')
+        .then(function(response){
+            setList(response.data);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    }
     return(
         <Faded>
             <div className="find-container">
@@ -12,10 +26,14 @@ function Find(){
                 <div className='find-box'>
                     <div id="plus-btn"><img src={plusImg} width='40'/></div>
                     <input type="text" placeholder="검색어를 입력하세요."/>
-                    <div id="search-btn"><img src={searchImg} width='40'/></div>
+                    <div id="search-btn" onClick={searchList}><img src={searchImg} width='40'/></div>
                 </div>
                 <div className='find-list'>
-
+                    {list.map((item)=>{
+                        return (
+                            <FindListItem restaurant={item}/>
+                        )
+                    })}
                 </div>
             </div>
         </Faded>
