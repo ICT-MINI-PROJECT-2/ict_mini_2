94148adc26892489ee13db2370a348f9ce8069d5
Gunmo Lee <rjsah5676@gmail.com>
2025-03-18 18:50:20 +0900

    디자인 해결

--- front/src/view/user/Singup.jsx
+++ front/src/view/user/Singup.jsx
@@ -32,8 +32,8 @@ function Signup(){
     });
 
     useEffect(() => {
-        //if(okChk.idOk && okChk.pwOk && okChk.pw_chkOk && okChk.emailOk && okChk.email2Ok && okChk.nameOk && okChk.zipcodeOk && okChk.zipcodeOk) { 
-            if(okChk.idOk) { 
+        if(okChk.idOk && okChk.pwOk && okChk.pw_chkOk && okChk.emailOk && okChk.email2Ok && okChk.nameOk && okChk.zipcodeOk && okChk.zipcodeOk) { 
+            
                 let userid = document.getElementById("userid");
                 let userpw = document.getElementById("userpw");
                 let username = document.getElementById("username");
