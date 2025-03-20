7c21dc055685ff28293f385b70f8231bb8111dc8
Gunmo Lee <rjsah5676@gmail.com>
2025-03-20 19:59:14 +0900

    맛집 추천
    ㅇㅇ

--- serv/src/main/java/com/ke/serv/service/RecommendService.java
+++ serv/src/main/java/com/ke/serv/service/RecommendService.java
@@ -2,7 +2,10 @@ package com.ke.serv.service;
 
 import com.ke.serv.entity.RestaurantEntity;
 import com.ke.serv.repository.RecommendRepository;
+import com.ke.serv.vo.PagingVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,5 +17,9 @@ public class RecommendService {
 
     public List<RestaurantEntity> findByCategoryAndLocation(String category, String location) {
         return repository.findByCategoryAndLocation(category, location);
+    }
+
+    public List<RestaurantEntity> findListByTag(List<String> a, String b) {
+        return repository.findRestByLocAndTag(a,b);
     }
 }
